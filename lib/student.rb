ea8ba2ea667dfe964a444ab9206e4ce82df10ecf
Makeda P Mckune <makedamckune@gmail.com>
2019-12-04 02:52:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,2 @@
-class Student  < User
-  
-  attr_accessor :knowledge
-  
-  def initialize(knowledge)
-    @knowledge = []
-  end 
-  
-  def learn(knowledge)
-    @knowledge << knowledge
-  end 
-  
-  def knowledge
-  
-  
-  
-end
+class Student < User 
+end 
